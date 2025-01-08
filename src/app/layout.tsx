872c67ba0477f1bcac45bc87bc7d1f717e790f6f
aamna-ansari <aamnansari29@gmail.com>
2025-01-08 20:50:10 +0500

    refactor layout and Navbar component; enhance page structure and add course links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+       <div className="relative w-full flex flex-col  justify-center top-8 ">
+       <Navbar/>
+       </div>
         {children}
       </body>
     </html>
