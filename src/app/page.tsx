5ca8a75806f25a9e8dcadf0ae7f7bdd2becc12e4
aamna-ansari <aamnansari29@gmail.com>
2025-01-08 21:39:41 +0500

    Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <main className="">
     <h1>
       Chai or code
     </h1>
