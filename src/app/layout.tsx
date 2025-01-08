6f8d5c6dd1989ce91d03b32b21f1810dadf88a3d
aamna-ansari <aamnansari29@gmail.com>
2025-01-08 20:59:53 +0500

    refactor layout; remove unnecessary top margin from container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-       <div className="relative w-full flex flex-col  justify-center top-8 ">
+       <div className="relative w-full flex flex-col  justify-center ">
        <Navbar/>
        </div>
         {children}
