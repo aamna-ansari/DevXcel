d9a2922e21faf9ed1be8fe7eacb2eee8e5e076df
aamna-ansari <aamnansari29@gmail.com>
2025-01-09 20:37:42 +0500

    update Navbar component to accept className prop and improve styling

--- src/compontents/Navbar.tsx
+++ src/compontents/Navbar.tsx
@@ -2,12 +2,14 @@
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
-const Navbar = () => {
+const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
   return (
-    <div>
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
         Navbar
-        </div>
+    </div>
   )
 }
 
