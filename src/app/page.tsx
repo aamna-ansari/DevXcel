5c0ec2c5c93387345ab885d8935c78417dd4f575
aamna-ansari <aamnansari29@gmail.com>
2025-01-09 15:22:25 +0500

    HeroSection components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HeroSection } from "@/compontents/HeroSection";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <h1 className="text-2xl text-center">Chai or code </h1>
+      <h1 className="text-center text-3xl text-red-400 ">Chai or code </h1>
       <HeroSection/>
     </main>
   );
