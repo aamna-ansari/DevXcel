fcb916c3b8a5ba456737b1501d5223bcada87698
aamna-ansari <aamnansari29@gmail.com>
2025-01-09 15:13:32 +0500

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { HeroSection } from "@/compontents/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <h1 className="text-2xl text-center">Chai or code </h1>
+      <HeroSection/>
     </main>
   );
 }
