169857f2017e955b7dc621e1b3cb4f525fcce0e9
aamna-ansari <aamnansari29@gmail.com>
2025-01-10 15:12:30 +0500

    Spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import HeroSection from "@/compontents/HeroSection";
 export default function Home() {
   return (
       <>
-      <h1 className="text-center text-2xl">Hello </h1>
       <HeroSection/>
       </>
     
