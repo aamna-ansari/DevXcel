4b0639ab88ccce90ad9c976612a5a6a74cce492a
aamna-ansari <aamnansari29@gmail.com>
2025-01-10 15:14:24 +0500

    Comment out Spotlight component in HeroSection || Spotlight also not worked

--- src/compontents/HeroSection.tsx
+++ src/compontents/HeroSection.tsx
@@ -6,7 +6,7 @@ import { Spotlight } from './ui/Spotlight'
 const HeroSection = () => {
   return (
     <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
-        <Spotlight/>
+        {/* <Spotlight/>  */} {/* Uncomment this line to see the spotlight animation */}
         <div className=' p-4 relative z-10 w-full text-center'>
         <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
