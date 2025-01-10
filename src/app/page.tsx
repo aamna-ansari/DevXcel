77e82b2900972b8cdef1943b844ee661e0c49241
aamna-ansari <aamnansari29@gmail.com>
2025-01-10 19:26:24 +0500

    WhyChooseUs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeatureCourses from "@/compontents/FeatureCourses";
 import HeroSection from "@/compontents/HeroSection";
+import WhyChooseUs from "@/compontents/WhyChooseUs";
 export default function Home() {
   return (
       <>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeatureCourses/>
+      <WhyChooseUs/>
       </main>
       </>
     
