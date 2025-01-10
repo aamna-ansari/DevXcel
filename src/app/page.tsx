67b362ea37bca73c84561e556e4ae627d801c513
aamna-ansari <aamnansari29@gmail.com>
2025-01-10 19:17:55 +0500

    FeatureCourses completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeatureCourses from "@/compontents/FeatureCourses";
 import HeroSection from "@/compontents/HeroSection";
 export default function Home() {
   return (
       <>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
+      <FeatureCourses/>
       </main>
       </>
     
